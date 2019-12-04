75a106942da612e31865facc16ea86d53bfb8c7e
Shahwar Spall <shevy.spall@gmail.com>
2019-12-04 22:00:04 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,13 +1,23 @@
 class Song
   attr_accessor :name, :artist_name
   @@all = []
+  
+  def initialize(name)
+    @name = name
+    save 
+  end 
 
   def self.all
     @@all
   end
 
   def save
-    self.class.all << self
+   @@all << self 
+  end 
+  
+  def self.create
+    @@all.new
   end
-
+    
+  
 end
