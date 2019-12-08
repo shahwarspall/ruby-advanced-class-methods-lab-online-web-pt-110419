0c9077980c90818e0a4fe6d32df34bfe3f14a140
Shahwar Spall <shevy.spall@gmail.com>
2019-12-08 21:16:57 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -68,9 +68,7 @@ class Song
   
    def self.create_from_filename(filename)
     
-    split = filename.split(" - ")
-    name = split[1].split(".")[0]
-    artist_name = split[0]
+ 
     
     song = self.create
     song.name = name
